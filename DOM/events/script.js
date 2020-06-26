0f577b781f639e51438946753fbc75daf916c05e
cihat <cihat02020202@gmail.com>
2020-06-26 20:39:34 +0300

    added  events

---
+++ DOM/events/script.js
@@ -0,0 +1,8 @@
+// Yontem 2
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World Yöntem 2");
+// Yontem 3 
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener('click', () => {
+  alert('Hello World Yöntem 3');
+});
