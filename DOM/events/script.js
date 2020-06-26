60199ba7b0685cc4746fcd8254b7c506ecd98fe6
cihat <cihat02020202@gmail.com>
2020-06-26 21:16:01 +0300

    added  events

--- DOM/events/script.js
+++ DOM/events/script.js
@@ -6,3 +6,31 @@ const btn2 = document.querySelector("#btn2");
 btn2.addEventListener('click', () => {
   alert('Hello World Yöntem 3');
 });
+// Farklı yontem1
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+// METHOD 2
+btn.onclick = alertFunction;
+
+// METHOD 3
+btn.addEventListener('click', alertFunction);
+
+btn.addEventListener('click', function (e) {
+  console.log(e);
+});
+
+const sec = document.querySelector('#buton');
+sec.addEventListener('click', () => {
+  // e.target.style.background = 'blue';
+  alert("alooooo!!");
+});
+
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', (e) => {
+    alert(button.id);
+  });
+});
