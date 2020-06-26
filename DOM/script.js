a8d8484ada64f10c607def44dfce7f062d2bff9d
cihat <cihat02020202@gmail.com>
2020-06-26 19:30:46 +0300

    added exercises and dom

--- DOM/script.js
+++ DOM/script.js
@@ -1,16 +1,28 @@
 // const container = document.querySelector('#container');
-const container = document.querySelector('body');
+
+const body = document.querySelector('body');
 const p = document.createElement('p');
 p.classList.add('p');
 p.textContent = 'Hey I’m red!';
 p.style.color = 'red';
-container.appendChild(p);
+body.appendChild(p);
 
 const h3 = document.createElement('h3');
 h3.classList.add('h3');
 h3.textContent = 'I\'m a blue h3!';
 h3.style.color = 'blue';
-container.appendChild(h3);
+body.appendChild(h3);
 
-const div = document.querySelector('div');
-div.classList.add('div');
+const denemeee = document.createElement('div');
+denemeee.classList.add('denemeee');
+denemeee.textContent = 'I\'m a blue denemeee!';
+denemeee.style.color = 'blue';
+body.appendChild(denemeee);
+
+// const div = document.querySelector('div');
+const pinkDiv = document.createElement('div');
+pinkDiv.classList.add('exercis');
+div.style.backgroundColor = pink;
+div.borderColor = black;
+div.textContent = 'deneme'
+body.appendChild(div);
