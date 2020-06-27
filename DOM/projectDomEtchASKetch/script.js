e471adde992cd31259187c299d43d594f99988ad
cihat <cihat02020202@gmail.com>
2020-06-27 17:19:48 +0300

    created project Dom Etch a sketch

--- DOM/projectDomEtchASKetch/script.js
+++ DOM/projectDomEtchASKetch/script.js
@@ -1,12 +1,40 @@
 const container = document.getElementById('container');
 
-function makeRows(rows , cols) {
+function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols) ; c++) {
+  for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement('div');
     cell.innerHTML = (c + 1);
     container.appendChild(cell).className = 'grid-item';
   };
+
+  cell[4].style.backgroundColor = blue;
 };
-makeRows(4,4);
+
+makeRows(4, 4);
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+document.getElementById('demo').addEventListener("mouseout", mouseOut);
+function mouseOver() {
+  document.getElementById('demo').style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById('demo').style.color = "black";
+}
+/*
+const grid = document.getElementsByClassName('grid-item');
+grid.addEventListener('mouseenter', function (event) {
+  event.target.style.color = purple;
+
+  setTimeout(function () {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+grid.addEventListener('mouseover', function (event) {
+  event.target.style.color = 'orange';
+  setTimeout(function () {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+*/
