727e25f58eb0ff8884048afae1256cef7e3e0f58
cihat <cihat02020202@gmail.com>
2020-06-27 11:46:08 +0300

    created community-dom

--- DOM/community-dom/script.js
+++ DOM/community-dom/script.js
@@ -1,5 +1,25 @@
+//section 1
 const body = document.querySelector('body');
 const pdeg = document.createElement('p');
 pdeg.textContent = 'Helloooooo';
 document.querySelector("p#demo").innerHTML = "Hello World!";
 body.appendChild(pdeg);
+//section 2
+let navLink = document.getElementById('nav');
+navLink.href = 'https://www.wikipedia.org';
+navLink.textContent = 'Navigate to Wikipedia';
+navLink.innerHTML = "changed my link is change";
+navLink.style.background = 'green';
+navLink.style.color = 'white';
+navLink.style.border.style = 'solid';
+//section 3
+const buton = document.createElement('button');
+buton.textContent = "BackGround Change";
+buton.style.fontSize = 'xx-large';
+body.appendChild(buton);
+let button = document.getElementById('changeBackgorund');
+buton.addEventListener('click', () => {
+  document.body.style.background = 'fuchsia';
+})
+//section 4
+document.getElementById("myImage").src = "https://www.istasyonsanat.com/upload/ckfinder/files/duyurular/resim-yapmanin-puf-naktalari.jpg";
