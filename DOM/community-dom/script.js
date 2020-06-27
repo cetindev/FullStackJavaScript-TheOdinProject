e43afd265990f8f277b374919558b4c4ebc77d09
cihat <cihat02020202@gmail.com>
2020-06-27 10:21:08 +0300

    created community-dom

---
+++ DOM/community-dom/script.js
@@ -0,0 +1,5 @@
+const body = document.querySelector('body');
+const pdeg = document.createElement('p');
+pdeg.textContent = 'Helloooooo';
+document.querySelector("p#demo").innerHTML = "Hello World!";
+body.appendChild(pdeg);
