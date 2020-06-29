e401783e7207d2f5caeccce197b9dea05133c982
cihat <cihat02020202@gmail.com>
2020-06-29 14:14:49 +0300

    completed css section in calculator project

--- WebDevelopment101/calculator/script/script.js
+++ WebDevelopment101/calculator/script/script.js
@@ -1,1 +1,15 @@
 //Script will be written here
+function add(firs,last) {
+  return firs + last;
+}
+function subtract(firs,last) {
+  return firs - last;
+}
+function multiply(firs,last) {
+  return firs * last;
+}
+function divide(firs,last) {
+  return firs / last;
+}
+
+const 
