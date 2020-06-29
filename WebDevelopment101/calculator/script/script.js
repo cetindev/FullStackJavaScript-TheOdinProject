30c1ab4df9421914257c1bff8af5974a7eb3dbb9
cihat <cihat02020202@gmail.com>
2020-06-29 17:26:39 +0300

    started js section in calculator project

--- WebDevelopment101/calculator/script/script.js
+++ WebDevelopment101/calculator/script/script.js
@@ -12,3 +12,14 @@ function divide(firs,last) {
   return firs / last;
 }
 
+const sifir = document.querySelector('.sifir');
+const bir = document.querySelector('.bir');
+const iki = document.querySelector('.iki');
+const uc = document.querySelector('.uc');
+const dort= document.querySelector('.dort');
+const bes = document.querySelector('.bes');
+const alti = document.querySelector('.alti');
+const yedi = document.querySelector('.yedi');
+const sekiz = document.querySelector('.sekiz');
+const dokuz = document.querySelector('.dokuz');
+
