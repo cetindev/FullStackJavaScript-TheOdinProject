fd131ac7274c765ec293669f60fad5d04feb25fb
cihat <cihat02020202@gmail.com>
2020-07-01 22:30:14 +0300

    remote

---
+++ WebDevelopment101/GulpProject/GulpProject/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+gulp.task('merhaba', function () {
+  return gulp.src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./'))
+})
+gulp.task('izle', function () {
+  gulp.watch('./scss/**/*.scss', ['merhaba'])
+})
--- WebDevelopment101/GulpProject/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')
-
-gulp.task('merhaba', function () {
-  return gulp.src('./scss/main".scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./'))
-})
