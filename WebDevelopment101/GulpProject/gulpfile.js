71b5f625b65872abd7c5cc91c4ce99b30033e7fd
cihat <cihat02020202@gmail.com>
2020-07-01 14:44:42 +0300

    created gulpProject in gulp.js

---
+++ WebDevelopment101/GulpProject/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+
+gulp.task('merhaba', function () {
+  return gulp.src('./scss/main".scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./'))
+})
