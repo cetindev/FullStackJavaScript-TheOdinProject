d2872ee938b934ddb7dc7cd004e96d43ad80b83f
cihat <cihat02020202@gmail.com>
2020-07-02 22:11:25 +0300

    added app.js, nodemodule and first interface FizzBuzz project

--- JavaScript/FizzBuzz/app.js
+++ JavaScript/FizzBuzz/app.js
@@ -8,9 +8,18 @@ rl.question("Lütfen Üst Sınırınızı Giriniz: ", function (answer) {
   console.log(`Üst sınırınız  ${answer}`)
   rl.close();
   for (let i = 1; i <= answer; i++) {
-    console.log(i);
-
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("FizzBuzz!");
+    }
+    else if (i % 3 == 0) {
+      console.log("Fizz!");
+    }
+    else if (i % 5 == 0) {
+      console.log("Buzz!");
+    }
+    else {
+      console.log(i);
+    }
   }
-
   process.exit(0);
 });
--- JavaScript/FizzBuzz/tempCodeRunnerFile.js
+++ JavaScript/FizzBuzz/tempCodeRunnerFile.js
@@ -1,1 +1,25 @@
-let answer = parseInt(prompt("Lütfen Üst Sınırı Giriniz: "));
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Lütfen Üst Sınırınızı Giriniz: ", function (answer) {
+  console.log(`Üst sınırınız  ${answer}`)
+  rl.close();
+  for (let i = 1; i <= answer; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("FizzBuzz!");
+    }
+    else if (i % 3 == 0) {
+      console.log("Fizz!");
+    }
+    else if (i % 5 == 0) {
+      console.log("Buzz!");
+    }
+    else {
+      console.log(i);
+    }
+  }
+  process.exit(0);
+});
