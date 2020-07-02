95172c5bc85907eb5d3e75bb36b8e3a581b68c67
cihat <cihat02020202@gmail.com>
2020-07-02 23:44:37 +0300

    completed app.js, nodemodule and first interface FizzBuzz project

--- JavaScript/FizzBuzz/app.js
+++ JavaScript/FizzBuzz/app.js
@@ -23,3 +23,18 @@ rl.question("Lütfen Üst Sınırınızı Giriniz: ", function (answer) {
   }
   process.exit(0);
 });
+
+/* const prompt = require("node_modules/")
+let answer = parseInt(prompt("Bir Sayı gir : "));
+
+for (let i = 1; i < answer; i++) {
+  if (i % 3 === 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+} */
