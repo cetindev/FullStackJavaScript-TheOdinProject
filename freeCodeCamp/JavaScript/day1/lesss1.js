ee61277949149e30ceaecdd40c06c0db48d9d53f
cihat <cihat02020202@gmail.com>
2020-07-03 14:30:56 +0300

    modified freeCodeCamp/JavaScpript

--- freeCodeCamp/JavaScript/day1/lesss1.js
+++ freeCodeCamp/JavaScript/day1/lesss1.js
@@ -135,3 +135,14 @@ Burada nameve agedeğişkenleri usernesneden ilgili değerlerinin değerleri olu
 
 Nesneden istediğiniz kadar çok veya az değer ayıklayabilirsiniz.
  */
+
+/* const LOCAL_FORECAST = {
+ yesterday: { low: 61, high: 75 },
+ today: { low: 64, high: 77 },
+ tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+// Only change code above this line
+*/
