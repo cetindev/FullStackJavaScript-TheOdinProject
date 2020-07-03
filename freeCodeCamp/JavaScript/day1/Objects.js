8ad16a82bebe10059936ae07a4c017869f9124b5
cihat <cihat02020202@gmail.com>
2020-07-03 21:58:40 +0300

    finished prototype

--- freeCodeCamp/JavaScript/day1/Objects.js
+++ freeCodeCamp/JavaScript/day1/Objects.js
@@ -331,4 +331,30 @@ var user2 = new User('Kadir Alkan', 34);
 console.log(user2.getFullName() + " " + user2.age());
  */
 
- 
+function Student() {
+}
+
+Student.prototype.sayName = function () {
+  console.log(this.name)
+}
+
+function EighthGrader(name) {
+  this.name = name;
+  this.grade = 8;
+}
+
+// don't do this!!!
+EighthGrader.prototype = Student.prototype;
+
+function NinthGrader(name) {
+  this.name = name;
+  this.grade = 9;
+};
+
+// noooo! not again!
+NinthGrader.prototype = Student.prototype
+
+NinthGrader.prototype.sayName = function () { console.log("HAHAHAHAHAHA") }
+
+const carl = new EighthGrader("carl")
+carl.sayName() //uh oh! this logs "HAHAHAHAHAHA" because we edited the sayName function!
