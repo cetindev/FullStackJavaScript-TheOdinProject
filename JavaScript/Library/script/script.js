b5e0ac68f4ae536d2c5d91cf72086d1e493fcba5
cihat <cihat02020202@gmail.com>
2020-07-04 17:56:32 +0300

    completed css style.css

--- JavaScript/Library/script/script.js
+++ JavaScript/Library/script/script.js
@@ -1,9 +1,24 @@
 //Script will be written here
-let myLibrary = [];
+/* let myLibrary = [];
 
 function Book() {
   
 }
 function addBookToLibrary() {
   
-}
+} */
+document.addEventListener('DOMContentLoaded', () => {
+  const newBookkForm = document.querySelector('#newBook');
+  const close = document.getElementById('close');
+  const button = document.querySelector('#addBook');
+  const ContainerR = document.querySelector('#booksRContainer');
+  const ContainerNr = document.querySelector('#booksNrContainer');
+  let forStorage = '';
+  let myLibrary = [];
+
+  showcase = localStorage.getItem('books');//checking storage
+
+  if (showcase != null) {
+    showcase = showcase.trim();
+  }
+})
