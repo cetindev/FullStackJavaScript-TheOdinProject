74902088c595bb14da0a6c5da2f9bbd970bfb537
cihat <cihat02020202@gmail.com>
2020-07-09 18:16:22 +0300

    finished createElements folder

--- JavaScript/Course/DOM/createElements/script.js
+++ JavaScript/Course/DOM/createElements/script.js
@@ -1,61 +1,35 @@
-//SElecting Elements
+//createing elements
+
+const li = document.createElement('li');
+
+//add class
+li.className = "list-group-item";
+
+//attribute
+li.setAttribute('title', 'creating new item');
+li.setAttribute('data-id', '5');
+
+//text node
+const text = document.createTextNode('Cihat Salik Github link');
+
+//alt eleman eklemek için 
+li.appendChild(text);
 
 
-//Single Elements
-//document.getElemetById()
-//document.querySelector()
-//Multiple Elements
-// class name
-//document.getElementsByClassName()
+//li'nin içindeki a etiketini oluşturmak
+const a = document.createElement('a');
+a.setAttribute('href', 'https://github.com/cihatdev');
 
-/* val = document.getElementsByClassName('list-group-item')[0];
-val = document.getElementsByClassName('list-group-item')[0];
-val = document.getElementsByClassName('list-group-item')[2];
-*/
-/* val = val[0]; */
-
-/* let val;
-val = document.getElementsByClassName('list-group-item');
-val[1].style.color = 'blue';
-val[1].style.fontSize = '25px';
-val[2].textContent = 'I will go to Germany';
-for (let i = 0; i < val.length; i++) {
-  console.log(val[i].style.color = "red");
-  console.log(val[i].textContent = "I will go to Germany");
-  }
-console.log(val);
- */
+a.className = 'delete-item float-right';
+a.innerHTML = '<i class="fab fa-github"></i>';
+a.style.fontSize = "24px";
+//append a to li
+li.appendChild(a);
+//append li to ul
+var ul = document.querySelector('#task-list');
+ul.appendChild(li);
 
 
-//document.getElementsByTagName()
 
-/* val = document.getElementsByTagName('li');
-val = document.getElementById('task-list').getElementsByTagName('a');
-*/
-
-//document.querySelectorAll()
-
-let val;
-/* val = document.querySelectorAll('li');
-
-val.forEach(function (item, index) {
-  item.textContent = `${index} - Hi`
-  //indexlerinnin elemenaların üzerine yazdıran kod
-}); */
-
-//even çift elemanları kapsa lyani 2 ve 4'ü kapsadı.
-//odd ise tekleri kapsar ve 1 ve 3'ü kapsadı
-val = document.querySelectorAll('li:nth-child(even)');
-val.forEach(function (item) {
-  item.style.background = '#ccc';
-})
-
-
-console.log(val);
-
-//Çokomelli
-//getElemnts.. ile başlayan propertyler HTML collictions olur ve bunlara
-//forEach() etki etmez sadaca for methodunun index numrası ile kullanabililriz.
-
-//querySelectorAll( ) kullandığımızda bize NodeList  geliyor ve bunu da 
-//forEach fonk.ile kullanabiliriz.
+console.log(li);
+console.log(a);
