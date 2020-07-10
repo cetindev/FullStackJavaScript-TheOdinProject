c8d36845da009e81ac83da7ffcf5dbac19c22df6
cihat <cihat02020202@gmail.com>
2020-07-10 16:32:52 +0300

    completed header section Css/responsive project

--- JavaScript/Course/DOM/mouseEvents/script.js
+++ JavaScript/Course/DOM/mouseEvents/script.js
@@ -1,1 +1,37 @@
 //Mouse Listener
+const btn = document.querySelector('#btnDeleteAll');
+const ul = document.querySelector('#task-list');
+// btn.addEventListener('click', eventHandler);
+// ul.addEventListener('click', eventHandler);
+
+//double click
+// btn.addEventListener('dblclick', eventHandler)
+
+/* 
+//mouse down 
+btn.addEventListener('mousedown', eventHandler);
+
+//mouse up 
+btn.addEventListener('mouseup', eventHandler);
+
+//mouseenter
+ul.addEventListener('mouseenter', eventHandler);
+//mouseleave
+ul.addEventListener('mouseleave', eventHandler);
+
+
+//mouseover
+ul.addEventListener('mouseover', eventHandler);
+//mouseout
+ul.addEventListener('mouseout', eventHandler);
+
+ */
+//mouse move
+const h5 = document.querySelector('h5');
+ul.addEventListener('mousemove', eventHandler);
+
+function eventHandler(e) {
+  console.log(`event type : ${e.type}`);
+
+  h5.textContent = `Mouse X: ${event.offsetX} Mouse Y: ${event.offsetY}`;
+}
