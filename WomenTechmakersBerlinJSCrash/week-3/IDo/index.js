d38ee2f3642df8ff639ec6a82f5f9c59db0a7b50
cihat <cihat02020202@gmail.com>
2020-07-16 18:38:48 +0300

    JS Crash Course Berlin Week3 async

--- WomenTechmakersBerlinJSCrash/week-3/IDo/index.js
+++ WomenTechmakersBerlinJSCrash/week-3/IDo/index.js
@@ -1,7 +1,7 @@
-const Person = require('./person')
-const Meetup = require('./meetup')
-const Chalk = require('chalk')
 const Database = require('./database')
+const Meetup = require('./meetup')
+/*
+const Chalk = require('chalk')
 
 const armagan = new Person("Armagan", 35)
 const mert = new Person("Mert", 34)
@@ -14,7 +14,15 @@ wtmb.printAttendeeNames()
 
 console.log(Chalk.blue.bgRed.bold(wtmb.name))
 
-Database.save('meetup.json', wtmb)
+Database.save('meetup.json', wtmb) */
 
 const loadedFile = Database.load('meetup.json')
-console.log(loadedFile.name)
+const Person = require('./person')
+const wtmb = Meetup.create(loadedFile);
+const omur = new Person('Omur', 30);
+const cihat = new Person('Cihat', 20);
+omur.attend(wtmb)
+cihat.attend(wtmb);
+Database.save('./meetup.json', wtmb);
+wtmb.printAttendeeNames();
+console.log(wtmb.name);
--- WomenTechmakersBerlinJSCrash/week-3/IDo/meetup.js
+++ WomenTechmakersBerlinJSCrash/week-3/IDo/meetup.js
@@ -1,13 +1,16 @@
 const Chalk = require('chalk')
 
 module.exports = class Meetup {
-  constructor(name) {
+  constructor(name, attendees = []) {
     this.name = name
-    this.attendees = []
+    this.attendees = attendees
   }
   printAttendeeNames() {
     this.attendees.forEach(printName)
   }
+  static create({ name, attendees }) {
+    return new Meetup(name, attendees);
+  }
 }
 
-const printName = person => console.log(Chalk.bgGreen(person.name))
+const printName = person => console.log(Chalk.bgRed(person.name))
