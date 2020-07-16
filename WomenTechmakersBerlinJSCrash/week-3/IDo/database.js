917a7d18cbdab248017dc5bd547fa29cd153fd3e
cihat <cihat02020202@gmail.com>
2020-07-16 22:04:22 +0300

    JS Crash Course Berlin Week3 async

--- WomenTechmakersBerlinJSCrash/week-3/IDo/database.js
+++ WomenTechmakersBerlinJSCrash/week-3/IDo/database.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 
-const save = function(filename, data) {
+const save = function (filename, data) {
   fs.writeFileSync(filename, JSON.stringify(data))
 }
 
-const load = function(filename) {
-  return JSON.parse(fs.readFileSync(filename, 'utf8'))
+const load = function (filename, callback) {
+  s.readFileSync(filename, 'utf8', (file) => {
+    callback(JSON.parse(file));
+  })
 }
 
 module.exports = { save, load }
