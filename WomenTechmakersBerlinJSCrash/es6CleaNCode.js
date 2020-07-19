6579b9d3927fa577f0cbd1e7d50998af795cbf4d
cihat <cihat02020202@gmail.com>
2020-07-19 23:34:43 +0300

    JS Crash Course Berlin create IDoWeek3 folder in file

---
+++ WomenTechmakersBerlinJSCrash/es6CleaNCode.js
@@ -0,0 +1,33 @@
+class Car {
+  constructor(make, model, color) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+  }
+
+  setMake(make) {
+    this.make = make;
+    // NOTE: Returning this for chaining
+    return this;
+  }
+
+  setModel(model) {
+    this.model = model;
+    // NOTE: Returning this for chaining
+    return this;
+  }
+
+  setColor(color) {
+    this.color = color;
+    // NOTE: Returning this for chaining
+    return this;
+  }
+
+  save() {
+    console.log(this.make, this.model, this.color);
+    // NOTE: Returning this for chaining
+    return this;
+  }
+}
+
+const car = new Car("Ford", "F-150", "red").setColor("pink").save();
