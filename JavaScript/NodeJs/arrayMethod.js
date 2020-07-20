1f7debb8024ed9a0bcb39482b9742fd1806a433a
cihat <cihat02020202@gmail.com>
2020-07-20 23:33:16 +0300

    added link mdn web docs)

--- JavaScript/NodeJs/arrayMethod.js
+++ JavaScript/NodeJs/arrayMethod.js
@@ -91,3 +91,18 @@ var result = array.some(function (piece) {
 
 console.log(result)
 */
+
+/* 
+var array = [1, 2, 3, 4, 5, 6]
+array.push(7) // Dizinin sonuna 7 elemanını ekler.
+array.shift() // Dizinin ilk elemanını çıkartır.
+array.unshift(0) // Diziye bir eleman ekler. (En başa)
+// delete array[1] // Dizinin 1. anahtarlı elemanını çıkartır.
+array.splice(2, 5) // Dizinin 2. elemanıyla 5. elemanı arasında kalan kısmını ayırır.
+array.concat([8, 9, 10]) // Diziyi başka bir dizi ile birleştirir.
+array.concat([11, 12, 13], [14, 15, 16]) // 3 diziyi birleştirir.
+array.slice(0) // Dizinin verilen anahtarlı değerini diziden ayırır
+ */
+
+//Daha fazla açıklama için aşağıdaki linke daha açıklayıcıdır.
+//    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
