aa5868a3969056efad6505b2151b0cbf98f61236
cihat <cihat02020202@gmail.com>
2020-07-20 23:08:39 +0300

    added every method(boolean)

---
+++ JavaScript/NodeJs/arrayMethod.js
@@ -0,0 +1,14 @@
+/*
+//Every metodu, dizinin tüm elemanlarının karşılaştırma operatörü aracılığıyla kontrol edilmesine yarar.
+//Sonuç mantıksaldır ( true, false ).
+
+var array = [1, 2, 3, 4, 5]
+
+var result = array.every(function (piece) {
+  if (piece < 6) {
+    return true
+  }
+})
+
+console.log(result)
+*/
