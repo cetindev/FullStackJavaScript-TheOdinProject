379c537f27a650e0732cff2c30219823816d1797
cihat <cihat02020202@gmail.com>
2020-07-20 23:09:07 +0300

    added filter method)

--- JavaScript/NodeJs/arrayMethod.js
+++ JavaScript/NodeJs/arrayMethod.js
@@ -12,3 +12,16 @@ var result = array.every(function (piece) {
 
 console.log(result)
 */
+
+/*
+//Filter metodu, dizinin elemanlarını filtrelemeyi sağlar. 
+//Sonuç yeni bir dizi olarak dönecektir. Ana dizinin elemanlarını değiştirmez.
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var result = array.filter(function (piece) {
+  if (piece % 2 === 0) {
+    return piece
+  }
+})
+
+console.log(result)
+*/
